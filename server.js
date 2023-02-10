30b4565b2958460f5061480691b041f045b5eb36
vinhlaem <vinhlaem2000@gmail.com>
2023-02-10 10:58:30 +0700

    feat: delete code 1

--- server.js
+++ server.js
@@ -21,13 +21,13 @@ app.get("/", (req, res) => {
 require("./app/routes/account.routes")(app);
 require("./app/routes/transaction.routes")(app);
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static("client/build"));
 
-  app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
-  );
-}
+//   app.get("*", (req, res) =>
+//     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+//   );
+// }
 
 const PORT = process.env.PORT || 5000;
 
